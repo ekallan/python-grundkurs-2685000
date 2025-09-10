bb62ba8cd3da625d07e1e99f2e0a8ddc6a1e91fb
Anna Kallias <anna.kallias@volkswagen.de>
2025-09-10 12:59:43 +0200

    did the exercises

---
+++ beispiel_code.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Bedingte Anweisungen in Python
+
+# Variablen
+x=10
+y=5
+
+# if-else-Bedingung
+if x <= y:
+  print ("x ist kleiner als y")
+else:
+  print ("x ist nicht kleiner als y")
+
+# if-elif-else-Bedingung
+if x == y:
+  print ("x ist gleich y")
+elif x > y:
+  print("x ist größer als y")
+else:
+  print ("x ist kleiner als y")
+
+
+
+z= 15
+if x>y and x<z:
+  print("x ist größer als y und kleiner als z")
+
+if x<y or x<z:
+  print("x ist entweder kleiner als  y oder kleiner als z")
---
+++ uebung.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+a = 2.0
+b = 2.0
+
+if a < b:
+  print(a ," ist kleiner als ", b)
+elif a == b:
+  print(a ," und ", b," sind gleich groß ")
+else:
+  print(b ," ist kleiner als ", a)
